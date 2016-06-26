4cdf48831e59d0374db61ff10c71ae613de6e9f8
flyAwayGG <flo2motion@gmail.com>
2016-06-26 16:42:56 +0300

    book

--- src/main/webapp/js/Controllers/BookController.js
+++ src/main/webapp/js/Controllers/BookController.js
@@ -4,7 +4,7 @@
     define([],
         function() {
 
-            var BookController = function($scope) {
+            var BookController = function($scope,$anchorScroll) {
 
 
                 var templates = [{ url: './partials/book/book_files/b_0.html' },
@@ -22,6 +22,51 @@
                     { url: './partials/book/book_files/b_12.html' },
                     { url: './partials/book/book_files/b_13.html' },
                     { url: './partials/book/book_files/b_14.html' },
+                    { url: './partials/book/book_files/b_15.html' },
+                    { url: './partials/book/book_files/b_16.html' },
+                    { url: './partials/book/book_files/b_17.html' },
+                    { url: './partials/book/book_files/b_18.html' },
+                    { url: './partials/book/book_files/b_19.html' },
+                    { url: './partials/book/book_files/b_21.html' },
+                    { url: './partials/book/book_files/b_22.html' },
+                    { url: './partials/book/book_files/b_23.html' },
+                    { url: './partials/book/book_files/b_24.html' },
+                    { url: './partials/book/book_files/b_25.html' },
+                    { url: './partials/book/book_files/b_26.html' },
+                    { url: './partials/book/book_files/b_27.html' },
+                    { url: './partials/book/book_files/b_28.html' },
+                    { url: './partials/book/book_files/b_29.html' },
+                    { url: './partials/book/book_files/b_30.html' },
+                    { url: './partials/book/book_files/b_31.html' },
+                    { url: './partials/book/book_files/b_32.html' },
+                    { url: './partials/book/book_files/b_33.html' },
+                    { url: './partials/book/book_files/b_34.html' },
+                    { url: './partials/book/book_files/b_35.html' },
+                    { url: './partials/book/book_files/b_36.html' },
+                    { url: './partials/book/book_files/b_37.html' },
+                    { url: './partials/book/book_files/b_38.html' },
+                    { url: './partials/book/book_files/b_39.html' },
+                    { url: './partials/book/book_files/b_40.html' },
+                    { url: './partials/book/book_files/b_41.html' },
+                    { url: './partials/book/book_files/b_42.html' },
+                    { url: './partials/book/book_files/b_43.html' },
+                    { url: './partials/book/book_files/b_44.html' },
+                    { url: './partials/book/book_files/b_45.html' },
+                    { url: './partials/book/book_files/b_46.html' },
+                    { url: './partials/book/book_files/b_47.html' },
+                    { url: './partials/book/book_files/b_48.html' },
+                    { url: './partials/book/book_files/b_49.html' },
+                    { url: './partials/book/book_files/b_50.html' },
+                    { url: './partials/book/book_files/b_51.html' },
+                    { url: './partials/book/book_files/b_52.html' },
+                    { url: './partials/book/book_files/b_53.html' },
+                    { url: './partials/book/book_files/b_54.html' },
+                    { url: './partials/book/book_files/b_55.html' },
+                    { url: './partials/book/book_files/b_56.html' },
+                    { url: './partials/book/book_files/b_57.html' },
+                    { url: './partials/book/book_files/b_58.html' },
+                    { url: './partials/book/book_files/b_59.html' },
+                    { url: './partials/book/book_files/b_60.html' },
                   
                 ];
                 $scope.template = templates[0];
@@ -33,7 +78,8 @@
 
                 $scope.openOgl = function() {
                     $scope.template = templates[0];
-                     buttonsController();
+                    openedPart = 0;
+                    buttonsController();
                 }
 
                 $scope.previous_part = function() {
@@ -41,12 +87,14 @@
                     openedPart -= 1;
                     $scope.template = templates[openedPart];
                     buttonsController();
+                    
                 }
 
                 $scope.next_part = function() {
                     openedPart += 1;
                     $scope.template = templates[openedPart];
                      buttonsController();
+
                 }
 
                 $scope.openPart = function(part_index) {
@@ -69,12 +117,13 @@
                     } else if (openedPart === (tmpl_len - 1)) {
                         $scope.showNextPartBtn = false;
                     }
+                    $anchorScroll();
                 };
 				buttonsController();
 
 
             };
-            return ["$scope", BookController];
+            return ["$scope","$anchorScroll", BookController];
         });
 
 
